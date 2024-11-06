39f5161b0d2ebd76428c7cf9a709cf8a8b59ab75
mominul-islam45 <mominul4586@gmail.com>
2024-11-07 00:23:00 +0600

    PostCss configured - Assignment started

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'jakarta': ['"Plus Jakarta Sans"', 'serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
